a3c9c488dac84ef0c45594999fbda5ba4e52a8bb
iyurin <ily2014yurin@gmail.com>
2019-01-15 17:19:48 +0300

    config chromedriver

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # selenium-test
+
+php ./vendor/bin/phpunit ./tests/AuthenticationTest
+
+wget http://selenium-release.storage.googleapis.com/3.14/selenium-server-standalone-3.14.0.jar
--- tests/common/CommonTest.php
+++ tests/common/CommonTest.php
@@ -3,6 +3,13 @@
 namespace DIDevelop\Tests\Selenium\Common;
 
 use Facebook\WebDriver\Chrome\ChromeDriver;
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Firefox\FirefoxDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\WebDriverCapabilityType;
+use Facebook\WebDriver\WebDriver;
+use Facebook\WebDriver\WebDriverPlatform;
 use PHPUnit\Framework\TestCase;
 
 class CommonTest extends TestCase
@@ -17,8 +24,25 @@ class CommonTest extends TestCase
 
     public function setUp()
     {
-        putenv("webdriver.chrome.driver=./bin/chromedriver");
-        $this->webDriver = ChromeDriver::start($this->desired_capabilities);
+/*        putenv("webdriver.chrome.driver=/Users/iyurin/app/src/php/di_develop_test/integrations_test/chromedriver");
+        $options = new Phan();
+        $options->setBinary('./bin/phantomjs');
+        $options->addArguments(array(   --webdriver=8080 --webdriver-selenium-grid-hub=http://142.93.159.120:4444
+            '--window-size=1280,1200', '--headless'
+        ));
+        $this->desired_capabilities = DesiredCapabilities::phantomjs();
+        $this->desired_capabilities->setCapability(ChromeOptions::CAPABILITY, $options);
+        $this->webDriver = ChromeDriver::start($this->desired_capabilities);*/
+
+        putenv("webdriver.chrome.driver=bin/chromedriver");
+        $this->desired_capabilities = DesiredCapabilities::chrome();
+        $this->desired_capabilities->setPlatform(WebDriverPlatform::LINUX);
+        $this->desired_capabilities->setCapability('acceptSslCerts', true);
+        $this->webDriver = RemoteWebDriver::create('http://142.93.159.120:4444/wd/hub', $this->desired_capabilities);
+
+        //putenv("webdriver.chrome.driver=/Users/iyurin/app/src/php/di_develop_test/integrations_test/geckodriver");
+        //$this->webDriver = ChromeDriver::start($this->desired_capabilities);
+
     }
 
     public function tearDown()
